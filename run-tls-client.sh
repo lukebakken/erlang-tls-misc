c68cd685a22edf7dcc22c1d6ce710a13cd62dc35
Luke Bakken <luke@bakken.io>
2021-04-14 17:12:36 -0700

    Write keylog data

--- run-tls-client.sh
+++ run-tls-client.sh
@@ -3,7 +3,16 @@
 set -e
 set -u
 
-echo 'FOO BAR BAZ' | openssl s_client -ign_eof -connect localhost:9999 \
+if command -v brew
+then
+    readonly openssl_bin="$(brew --prefix openssl)/bin/openssl"
+    readonly openssl_xtra_args="-tls1_3"
+else
+    readonly openssl_bin='openssl'
+    readonly openssl_xtra_args="-tls1_3"
+fi
+
+echo 'FOO BAR BAZ' | "$openssl_bin" s_client "$openssl_xtra_args" -ign_eof -connect localhost:9999 \
     -CAfile ./tls-gen/basic/result/ca_certificate.pem \
     -cert ./tls-gen/basic/result/client_certificate.pem \
     -key ./tls-gen/basic/result/client_key.pem -servername "$(hostname)"
--- src/tls_server.erl
+++ src/tls_server.erl
@@ -11,42 +11,56 @@ start() ->
         {reuseaddr, true},
         {sni_fun, fun tls_server:sni_fun/1}
     ],
-    io:format("[INFO] before ssl:listen(9999, Opts)~n", []),
+    ok = io:format("[INFO] before ssl:listen(9999, Opts)~n", []),
     {ok, ListenSocket} = ssl:listen(9999, Opts),
-    io:format("[INFO] after ssl:listen(9999, Opts)~n", []),
-    io:format("[INFO] before ssl:transport_accept~n", []),
+    ok = io:format("[INFO] after ssl:listen(9999, Opts)~n", []),
+    ok = io:format("[INFO] before ssl:transport_accept~n", []),
     {ok, TLSTransportSocket} = ssl:transport_accept(ListenSocket),
-    io:format("[INFO] after ssl:transport_accept~n", []),
-    io:format("[INFO] before ssl:handshake~n", []),
+    ok = io:format("[INFO] after ssl:transport_accept~n", []),
+    ok = io:format("[INFO] before ssl:handshake~n", []),
     Socket =
         case ssl:handshake(TLSTransportSocket) of
             {ok, S, Ext} ->
-                io:format("[INFO] ssl:handshake Ext: ~p~n", [Ext]),
+                ok = io:format("[INFO] ssl:handshake Ext: ~p~n", [Ext]),
                 S;
             {ok, S} ->
                 S;
             Error ->
-                io:format("[ERROR] ssl:handshake Error: ~p~n", [Error]),
-                init:stop()
+                ok = io:format("[ERROR] ssl:handshake Error: ~p~n", [Error]),
+                ok = init:stop()
         end,
-    io:format("[INFO] after ssl:handshake~n", []),
-    io:format("[INFO] ssl:handshake sni_hostname: ~p~n", [get_sni_hostname(Socket)]),
+    ok = io:format("[INFO] after ssl:handshake~n", []),
+    ok = io:format("[INFO] ssl:handshake sni_hostname: ~p~n", [get_sni_hostname(Socket)]),
+    write_keylog(Socket),
     loop(Socket).
 
 loop(Socket) ->
-    io:format("[INFO] top of loop/2~n", []),
+    ok = io:format("[INFO] top of loop/2~n", []),
     receive
         Data ->
-            io:format("Data: ~p~n", [Data]),
+            ok = io:format("[INFO] Data: ~p~n", [Data]),
             loop(Socket)
     after 5000 ->
-        io:format("DONE!~n", []),
-        ssl:close(Socket),
-        init:stop()
+        ok = io:format("[INFO] DONE!~n", []),
+        ok = ssl:close(Socket),
+        ok = init:stop()
     end.
 
+write_keylog(Socket) ->
+    ok = io:format("[INFO] writing keylog data to keylog.bin~n", []),
+    KeylogItems1 =
+        case ssl:connection_information(Socket, [keylog]) of
+            {ok, [{keylog, KeylogItems0}]} ->
+                KeylogItems0;
+            {ok, []} ->
+                ok = io:format("[WARNING] no keylog data!~n", []),
+                []
+        end,
+    ok = file:write_file("keylog.bin", [[KeylogItem, $\n] || KeylogItem <- KeylogItems1]),
+    ok.
+
 sni_fun(ServerName) ->
-    io:format("[INFO] sni_fun ServerName: ~p~n", [ServerName]),
+    ok = io:format("[INFO] sni_fun ServerName: ~p~n", [ServerName]),
     [].
 
 get_sni_hostname(Socket) ->
@@ -56,6 +70,6 @@ get_sni_hostname(Socket) ->
         {ok, [{sni_hostname, Hostname}]} ->
             Hostname;
         Error ->
-            io:format("[ERROR] ssl:connection_information Error: ~p~n", [Error]),
+            ok = io:format("[ERROR] ssl:connection_information Error: ~p~n", [Error]),
             undefined
     end.
