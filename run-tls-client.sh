a33e0b05f8ee20b09d41aca5bef36c28a328831f
Luke Bakken <luke@bakken.io>
2022-03-31 15:54:44 -0700

    Update tls-gen
    Updates for latest tls-gen
    
    Try to get CRLs working
    
    Add custom cache module that caches HTTP responses
    
    More improvements to make CRLs into account
    
    Add redbug into the mix, caching works now. No way to inspect or clear the cache yet.

--- run-tls-client.sh
+++ run-tls-client.sh
@@ -12,7 +12,9 @@ else
     readonly openssl_xtra_args="-tls1_3"
 fi
 
+readonly hostname="$(hostname)"
+
 echo 'FOO BAR BAZ' | "$openssl_bin" s_client "$openssl_xtra_args" -ign_eof -connect localhost:9999 \
     -CAfile ./tls-gen/basic/result/ca_certificate.pem \
-    -cert ./tls-gen/basic/result/client_certificate.pem \
-    -key ./tls-gen/basic/result/client_key.pem -servername "$(hostname)"
+    -cert "./tls-gen/basic/result/client_${hostname}_certificate.pem" \
+    -key "./tls-gen/basic/result/client_${hostname}_key.pem" -servername "$hostname"
